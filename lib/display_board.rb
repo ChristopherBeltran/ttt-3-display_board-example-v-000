785cd155fddfd3c38cc085ba06691301e8d49c16
Christopher Beltran <christod.beltran@gmail.com>
2018-06-02 22:53:24 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
  def display_board
-irb(main):002:1> puts "   |   |   "
-irb(main):003:1> puts "-----------"
+puts "   |   |   "
+puts "-----------"
 irb(main):004:1> puts "   |   |   "
 irb(main):005:1> puts "-----------"
 irb(main):006:1> puts "   |   |   "
